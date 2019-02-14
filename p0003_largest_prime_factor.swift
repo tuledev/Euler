88d05fd6664854f487a612be4f2a58430d69cfa5
Tu Le <tule.developer@gmail.com>
2019-02-14 11:43:39 +0700

    add question

--- p0003_largest_prime_factor.swift
+++ p0003_largest_prime_factor.swift
@@ -1,4 +1,7 @@
-
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143 ?
+//
 func largestPrimeNumberOf(number: Int) -> Int {
   var currentNumber = number
   var lastestPrime = 1
